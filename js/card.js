f59da270d554ceb86a724ed8993ea8e2089b28cc
Akanksha Singh <akankshasingh872@gmail.com>
2021-02-03 21:19:07 +0530

    added basic form structure and initial values

---
+++ js/card.js
@@ -0,0 +1,19 @@
+
+
+function setInitialPlaceholder(){
+    let div = document.getElementById('user-name');
+    div.setAttribute("class", "white-text");
+    div.innerHTML ="YOUR NAME HERE";
+
+    div = document.getElementById('card-cvv');
+    div.setAttribute("class", "white-text");
+    div.innerHTML ="...";
+
+    div = document.getElementById('card-number');
+    div.setAttribute("class", "white-text");
+    div.innerHTML =".... .... .... ....";
+
+    div = document.getElementById('card-date');
+    div.setAttribute("class", "white-text");
+    div.innerHTML ="../..";
+}
